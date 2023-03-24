217b3bb2b35cb092bc9040b72791492def18d747
dan-kc <63171098+dan-kc@users.noreply.github.com>
2023-03-24 15:44:12 +0000

    Added onboarding button

--- app/components/ClusterList.tsx
+++ app/components/ClusterList.tsx
@@ -14,32 +14,46 @@ const ClusterList: React.FC<Props> = ({ clusters }) => {
   const [addingCluster, setAddingCluster] = useState<boolean>(false);
   return (
     <section className="flex flex-col gap-2">
-      {clusters ? (
-        clusters.map((cluster) => {
-          const { id, name, start_time, end_time, habits } =
-            cluster;
-          return (
-            <Cluster
-              id={id}
-              key={id}
-              name={name}
-              startTime={start_time}
-              endTime={end_time}
-              habits={habits}
-            />
-          );
-        })
-      ) : (
-        <p> Add your first cluster </p>
-      )}
+      {clusters.map((cluster) => {
+        const { id, name, start_time, end_time, habits } =
+          cluster;
+        return (
+          <Cluster
+            id={id}
+            key={id}
+            name={name}
+            startTime={start_time}
+            endTime={end_time}
+            habits={habits}
+          />
+        );
+      })}
       <Dialog.Root open={addingCluster} onOpenChange={setAddingCluster}>
-        <div className="flex justify-end">
-          <Dialog.Trigger asChild>
-            <button className="flex h-10 w-10 justify-center rounded-full bg-violetDark-3">
-              <PlusIcon className="h-5 w-5 self-center text-violetDark-11" />
-            </button>
-          </Dialog.Trigger>
-        </div>
+
+
+        {
+          clusters.length === 0 ?
+            <Dialog.Trigger asChild>
+              <button className="rounded-md border text-2xl font-semibold border-violetDark-6 bg-violetDark-3 py-10 text-violetDark-11 flex justify-center  text-center">
+                Add your first cluster!
+              </button>
+            </Dialog.Trigger>
+            :
+            <div className="flex justify-start">
+              <Dialog.Trigger asChild>
+                <button className="flex h-10 w-10 justify-center rounded-full bg-violetDark-3">
+                  <PlusIcon className="h-5 w-5 self-center text-violetDark-11" />
+                </button>
+              </Dialog.Trigger>
+            </div>
+
+        }
+
+
+
+
+
+
         <EditClusterDialog
           isNew
           id={uuidv4()}
